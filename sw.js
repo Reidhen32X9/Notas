b0c05a858ce4c00cc450399bca47d8f48c8dd377
Reidhen32X9 <kevindeckster9@gmail.com>
2026-02-13 13:37:45 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'caderno-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalando Service Worker e salvando cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Interceptando requisições para usar cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Atualizando cache
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
+  );
+});
